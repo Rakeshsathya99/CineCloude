73fb71687b715b4ae9b0cb8725a7e0b7a2ce6e12
Rakeshsathya99 <srakeshshetty7@gmail.com>
2025-12-22 14:22:20 +0530

    smtp server error fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -143,27 +143,29 @@ const sendBookingConfirmationEmail = inngest.createFunction(
   { event: "app/show.booked" },
   async ({ event, step }) => {
     const { bookingId } = event.data;
-    const booking = await Booking.findById(bookingId).populate('user').populate({
-      path: 'show',
-      populate: {path: 'movie', model:'Movie'}
+    
+    const booking = await step.run("fetch-booking-details", async () => {
+      return await Booking.findById(bookingId).populate('user').populate({
+        path: 'show',
+        populate: {path: 'movie', model:'Movie'}
+      })
     })
     
-    await sendEmail({
-        to: booking.user.email,
-        subject: `Ticket Booking Confirmation:"${booking.show.movie.title}" booked!`,
-        body:`<div style="font-family: Arial, sans-serif; line-height: 1.5;">
-                <h2>Hi ${booking.user.name},</h2>
-              <p>Your booking for <strong style="color: #F84565;">"$
-              {booking.show.movie.title}"</strong> is confirmed.</p>
-              <p>
-                  <strong>Date:</strong> ${new Date(booking.show.
-                  showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}<br/>
-                  <strong>Time:</strong> ${new Date(booking.show.
-                  showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
-              </p>
-              <p>Enjoy the show!🍿 </p>
-              <p>Thanks for booking with us! <br/>- QuickShow Team</p>
-                </div>`
+    await step.run("send-confirmation-email", async () => {
+      await sendEmail({
+          to: booking.user.email,
+          subject: `Ticket Booking Confirmation:"${booking.show.movie.title}" booked!`,
+          body:`<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+                  <h2>Hi ${booking.user.name},</h2>
+                <p>Your booking for <strong style="color: #F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
+                <p>
+                    <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}<br/>
+                    <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
+                </p>
+                <p>Enjoy the show!🍿 </p>
+                <p>Thanks for booking with us! <br/>- QuickShow Team</p>
+                  </div>`
+      })
     })
   }
 )
